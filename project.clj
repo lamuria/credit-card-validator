7ee5d6aaea482a13b33aca1d3c18ce55b680d18d
Marcos <marcoslamuria@gmail.com>
2015-05-16 00:00:35 +0100

    add result lib

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-time "0.9.0"]]
+                 [clj-time "0.9.0"]
+                 [clanhr/result "0.2.0"]]
   :plugins [[com.jakemccrary/lein-test-refresh "0.9.0"]])
